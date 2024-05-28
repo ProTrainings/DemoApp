da7f1207d50b155aa26531e8849f488e5b4e0151
Tim Bugai <tim@bugai.net>
2024-05-28 17:59:52 -0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Tim Bugai on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
